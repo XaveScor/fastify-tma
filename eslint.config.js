8b053d234c85fce0d158071090e4a0b6a063aec8
Viktor Zdorovtsev <v.zdvacew@gmail.com>
2024-11-24 20:29:33 +0700

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,15 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu({
+  typescript: {
+    overridesTypeAware: {
+      'ts/no-floating-promises': ['error', {
+        allowForKnownSafePromises: [
+          { from: 'package', name: 'FastifyInstance', package: 'fastify' },
+          { from: 'package', name: 'FastifyReply', package: 'fastify' },
+          { from: 'package', name: 'SafePromiseLike', package: 'fastify' },
+        ],
+      }],
+    },
+  },
+})
